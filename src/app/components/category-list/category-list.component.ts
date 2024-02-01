7d734e3aa0063ea285dfe77cf7be04f3259c95dd
khushal.roljack@gmail.com <akshatroljack@gmail.com>
2024-02-01 18:01:21 +0530

    category-list

--- src/app/components/category-list/category-list.component.ts
+++ src/app/components/category-list/category-list.component.ts
@@ -1,12 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
+import { environment } from '../../../environment/environment';
 
 @Component({
   selector: 'app-category-list',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule],
   templateUrl: './category-list.component.html',
   styleUrl: './category-list.component.css',
 })
-export class CategoryListComponent {}
+export class CategoryListComponent {
+  categories: any = [];
+  fileUrl: string = environment.fileUploadUrl;
+
+  constructor(public apiservice: ApiService) {
+    this.getCategories();
+  }
+
+  getCategories() {
+    console.log('in con get categories');
+    this.apiservice.getMethod('home-categories').subscribe({
+      next: (resp) => {
+        this.categories = resp;
+        console.log('in con dsffasdasd');
+      },
+      error: (e) => {
+        console.error(e, 'error');
+      },
+      complete: () => console.info('complete'),
+    });
+  }
+}
